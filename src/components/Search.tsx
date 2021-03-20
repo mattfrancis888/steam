66343abc7a2605d60058fdfe4b5f358fa5b9522b
Matthew Francis <mattfrancis888@gmail.com>
2021-03-20 14:44:19 -0400

    Able to hover over games and show preview.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,77 +1,186 @@
 import React, { useEffect, useState, useRef } from "react";
+import { fetchGames, fetchDiscountedGames, Game } from "../actions";
+import { connect } from "react-redux";
+import { StoreState } from "../reducers";
+import { GamesStateResponse } from "../reducers/gamesReducer";
+import { ErrorStateResponse } from "reducers/errorReducer";
+import Loading from "./Loading";
+import history from "../browserHistory";
+import moment from "moment";
+import _ from "lodash";
+interface SearchProps {
+    games: GamesStateResponse;
+    discountedGames: GamesStateResponse;
+    errors: ErrorStateResponse;
+    fetchGames(): void;
+    fetchDiscountedGames(): void;
+}
 
-const Search: React.FC<{}> = (props) => {
-    // const renderScreenshotsForGame = (gameId: number) => {
-    //     let game = _.filter(props.games.data?.games, {
-    //         game_id: gameId,
-    //     });
-    //     let maxScreenshotToShow = 4;
-    //     return game[0].screenshots.map((screenshot, index) => {
-    //         if (index < maxScreenshotToShow)
-    //             return <img key={index} src={screenshot} alt="preview"></img>;
-    //     });
-    // };
+const Search: React.FC<SearchProps> = (props) => {
+    useEffect(() => {
+        props.fetchGames();
+        // props.fetchDiscountedGames();
+    }, []);
 
-    // const renderGenresForGameText = (gameId: number) => {
-    //     let filteredContent = _.filter(props.games.data?.games, {
-    //         game_id: gameId,
-    //     });
+    const [hoverData, setHoverData] = useState(1);
 
-    //     return filteredContent.map((content, index) => {
-    //         return content.genres.map((genre, index) => {
-    //             if (content.genres.length === index + 1) {
-    //                 return ` ${genre}`;
-    //             }
-    //             return ` ${genre},`;
-    //         });
-    //     });
-    // };
+    const renderScreenshotsForGame = (gameId: number) => {
+        let game = _.filter(props.games.data?.games, {
+            game_id: gameId,
+        });
+        let maxScreenshotToShow = 1;
+        return game[0].screenshots.map((screenshot, index) => {
+            if (index < maxScreenshotToShow)
+                return <img key={index} src={screenshot} alt="preview"></img>;
+        });
+    };
 
-    return (
-        <div className="searchContainer">
-            <div className="searchRowsColumn">
-                <div className="searchRow">
-                    <div className="searchRowImageAndTitleWrap">
-                        <img
-                            src="https://cdn.akamai.steamstatic.com/steam/apps/1174180/capsule_sm_120.jpg?t=1615608871 1x, https://cdn.akamai.steamstatic.com/steam/apps/1174180/capsule_231x87.jpg?t=1615608871 2x"
-                            alt="game"
-                        ></img>
-                        <p className="searchGameTitle">RDR2</p>
-                    </div>
+    const renderGenresForGameText = (gameId: number) => {
+        let filteredContent = _.filter(props.games.data?.games, {
+            game_id: gameId,
+        });
 
-                    <p className="searchRowDate">5 Dec 2019</p>
-                    <div className="searchRowPriceWrap">
-                        <div className="searchRowDiscount">-10%</div>
-                        <div className="searchRowPriceTextWrap">
-                            <p className="searchRowPriceStriked">18.99</p>
-                            <p className="searchRowPrice">20.99</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        return filteredContent.map((content, index) => {
+            return content.genres.map((genre, index) => {
+                return <p className="searchGameGenres">{genre}</p>;
+            });
+        });
+    };
 
+    const renderChartGamePreview = (gameId: number) => {
+        let filteredContent = _.filter(props.games.data?.games, {
+            game_id: gameId,
+        });
+
+        return (
             <div className="searchPreviewHover">
                 <p className="searchGamePreviewTitle">
-                    {/* {filteredContent.map((content, index) => {
-                                return content.title;
-                            })} */}
-                    RDR2
+                    {filteredContent.map((content, index) => {
+                        return content.title;
+                    })}
                 </p>
 
                 <div className="chartGamePreviewScreenshots">
-                    <img
-                        src={
-                            "https://cdn.cloudflare.steamstatic.com/steam/apps/518790/ss_8a75aa823fbaa3a8ec3cf844d1340308250737a4.600x338.jpg?t=1607595944 "
-                        }
-                        alt="preview"
-                    ></img>
+                    {renderScreenshotsForGame(gameId)}
                     <div className="searchGameGenresWrap">
-                        <p className="searchGameGenres">Action</p>
+                        {renderGenresForGameText(gameId)}
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    };
+
+    const renderPrice = (content: Game) => {
+        if (parseFloat(content.discount_percentage) > 0) {
+            return (
+                <React.Fragment>
+                    <div className="searchGameDiscount">
+                        -{parseFloat(content.discount_percentage) * 100}%
+                    </div>
+                    <div className="searchGamePriceTextWrap">
+                        <p className="searchGamePriceStriked">
+                            ${parseFloat(content.price).toFixed(2)}
+                        </p>
+                        <p className="searchGamePrice">
+                            $
+                            {parseFloat(content.price_after_discount).toFixed(
+                                2
+                            )}
+                        </p>
+                    </div>
+                </React.Fragment>
+            );
+        } else {
+            //no discount
+            return (
+                <p className="searchGamePrice">
+                    ${parseFloat(content.price).toFixed(2)}
+                </p>
+            );
+        }
+    };
+
+    const renderChartGames = () => {
+        let contentToRender = props.games.data?.games;
+        // if (specialsTabClicked === true) {
+        //     contentToRender = props.discountedGames.data?.games;
+        // }
+        if (contentToRender)
+            return contentToRender.map((content, index) => {
+                return (
+                    <div
+                        key={index}
+                        // className={`searchGame  ${
+                        //     hoverData === content.game_id
+                        //         ? "searchGameContainerToggled"
+                        //         : ""
+                        // }`}
+                        className={`searchGameContainer ${
+                            hoverData === content.game_id
+                                ? "searchGameContainerToggled"
+                                : ""
+                        }`}
+                        onClick={() => {
+                            history.push(`game/${content.game_id}`);
+                        }}
+                        onMouseOver={() => {
+                            setHoverData(content.game_id);
+                        }}
+                    >
+                        <div className="searchGameImageAndTitleWrap">
+                            <img src={content.cover_url} alt="game"></img>
+                            <p className="searchGameTitle">{content.title}</p>
+                        </div>
+
+                        <div className="searchGameDateAndPriceWrap">
+                            <p className="searchGameDate">
+                                {moment(content.release_date).format(
+                                    "YYYY/MM/DD"
+                                )}
+                            </p>
+                            {renderPrice(content)}
+                        </div>
+                    </div>
+                );
+            });
+    };
+
+    const renderContent = () => {
+        if (props.errors.data?.error) {
+            return (
+                <div className="serverErrorContainer">
+                    <h3 className="serverErrorText">
+                        {props.errors.data?.error}
+                    </h3>
+                </div>
+            );
+        } else if (props.games.data) {
+            return (
+                <div className={`searchContainer`}>
+                    <div className="searchGamesColumn">
+                        {renderChartGames()}
+                    </div>
+
+                    {renderChartGamePreview(hoverData)}
+                </div>
+            );
+        } else {
+            return <Loading />;
+        }
+    };
+
+    return <React.Fragment>{renderContent()}</React.Fragment>;
 };
 
-export default Search;
+const mapStateToProps = (state: StoreState) => {
+    return {
+        games: state.games,
+        discountedGames: state.discountedGames,
+        errors: state.errors,
+    };
+};
+
+export default connect(mapStateToProps, {
+    fetchGames,
+    fetchDiscountedGames,
+})(Search);
